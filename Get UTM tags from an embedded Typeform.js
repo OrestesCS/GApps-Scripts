fa9b9f191f8b6828b66080c7dc62c5c9eadff6ed
OrestesCS <60467011+OrestesCS@users.noreply.github.com>
2020-05-17 14:50:25 +0200

    Update and rename Get UTM tags from an embedded Typeform to Get UTM tags from an embedded Typeform.js

--- Get UTM tags from an embedded Typeform.js
+++ Get UTM tags from an embedded Typeform.js
@@ -1,7 +1,7 @@
-You will need to load jquery, this one or latest version:
+//You will need to load jquery, this one or latest version:
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
-Then you basically just take the URL in js and split the part you want and then replace the URL that loads the embeded form to pass the UTM tag
+//Then you basically just take the URL in js and split the part you want and then replace the URL that loads the embeded form to pass the UTM tag
 of the initial website : 
 
  <script>
